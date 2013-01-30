84edadfeefb129d53f9f85c7f0cac17371506bfc
Chen Guo <chen.guo.0625@gmail.com>
2013-01-30 03:37:47 -0800

    Significant refactoring. Properly track online users count.

--- chatServer.js
+++ chatServer.js
@@ -1,76 +1,138 @@
 // Chat server
 (function() {
+    var users = { nUsers: 0 };
+    var nUsers = 0;
+
     exports.init = function (server) {
         // Instantiate Socket.IO
         var io = require('socket.io');
         io = io.listen(server);
-
-        var users = {};
-        var nUsers = 0;
+        io.set('log level', 1);
 
         // Connection
         io.sockets.on('connection', function(socket) {
-            // Send welcome message
-            socket.emit('welcome', { msg: 'Connected to server',
-                                     online: nUsers });
+            msgConn(socket);
+        });
+    };
 
-            // Enter chat notice
-            socket.on('id', function(data) {
-                data.online = nUsers;
+    // Handle connection message.
+    var msgConn = function(socket) {
 
-                // If user has a pending timeout, then user
-                // was very recently logged on. Cancel the
-                // timeout and don't issue an "enter" message.
-                if (users[data.user])
-                {
-                    clearTimeout(users[data.user])
-                }
-                else
-                {
-                    nUsers++;
-                    data.online++;
-                    socket.broadcast.emit('enter', data);
-                    console.log("User " + data.user + " joined. "
-                                + nUsers + " user(s) online.");
-                }
-                socket.emit('accepted', data);
-                socket.set('name', data.user);
-                delete(users[data.user]);
-            });
+        // Save connection parameters
+        var addr = socket.handshake.address.address + ":"
+            + socket.handshake.address.port;
+        socket.set('addr', addr);
+        console.log("Connection from " + addr);
 
-            // Incoming message
-            socket.on('message', function(data) {
-                console.log(data);
-                data.msg = data.msg.replace(/</, '&#60');
-                data.msg = data.msg.replace(/>/, '&#62');
-                socket.get('name', function(err, name) {
-                    data.user = name;
-                    socket.emit('echo', data);
-                    socket.broadcast.emit('message', data);
-                });
-            });
+        // Send welcome message
+        socket.emit('welcome', { msg: 'Connected to server',
+                                 online: nUsers });
 
-            // Disconnect
-            socket.on('disconnect', function() {
-                socket.get('name', function(err, name) {
-                    // If no disconnect message is pending
-                    // send it after 5 seconds.
-                    if (!users[name])
-                    {
-                        var discMsg = function() {
-                            nUsers--;
-                            var data = { user: name, online: nUsers };
-                            socket.broadcast.emit('disconnect', data);
+        // Set function handlers.
+        socket.on('id', function(data) {
+            msgID(data, socket);
+        });
 
-                            delete users[name];
-                            console.log("User " + name + " left. "
-                                        + nUsers + " user(s) online.");
-                        };
-                        users[name] = setTimeout(discMsg, 5000);
-                    }
-                });
+
+        // Incoming message
+        socket.on('message', function(data) {
+            console.log(data);
+            data.msg = data.msg.replace(/</, '&#60');
+            data.msg = data.msg.replace(/>/, '&#62');
+            socket.get('key', function(err, key) {
+                data.user = users[key].name;
+                socket.emit('echo', data);
+                socket.broadcast.emit('message', data);
             });
         });
 
+        // Disconnect
+        socket.on('disconnect', function() {
+            msgDisc(socket);
+        });
+    };
+
+    // Handle ID message.
+    var msgID = function (data, socket) {
+        socket.get('addr', function (err, addr) {
+            var key = addr + ':' + data.user;
+
+            console.log('ID: ' + key);
+
+            // If this is new log on, initialize.
+            if (users.hasOwnProperty(key) === false)
+            {
+                users.nUsers++;
+                users[key] = { name: data.user,
+                               instances: 0 };
+
+                // Acknowledge ID message.
+                data.online = users.nUsers;
+                socket.broadcast.emit('enter', data);
+
+                console.log('New connection: ' + users.nUsers + ' users');
+            }
+            // Record of previous log on exists.
+            else
+            {
+                // If user has a pending timeout, then user
+                // was very recently logged on. Cancel the
+                // timeout and don't issue an "enter" message.
+                if (users[key].hasOwnProperty('timeout'))
+                {
+                    clearTimeout(users[key].timeout)
+                    delete users[key].timeout;
+                    console.log('Reconnection');
+                }
+                else
+                {
+                    console.log('Duplicate logon');
+                }
+            }
+
+            data.online = users.nUsers;
+            socket.emit('accepted', data);
+            socket.set('key', key);
+            incrInstance(key);
+        });
+    };
+
+    // Handle disconnect message
+    var msgDisc = function (socket) {
+        socket.get('key', function(err, key) {
+            // If no disconnect message is pending
+            // send it after 5 seconds.
+            if (!users[key].hasOwnProperty('timeout'))
+            {
+                var cb = function() {
+                    socket.broadcast.emit('disconnect',
+                                          { user: users[key].name,
+                                            online: users.nUsers });
+                    delete users[key].timeout;
+                    if (decrInstance(key) == 0)
+                    {
+                        delete users[key];
+                        users.nUsers--;
+                        console.log("User " + key + " left. "
+                                    + users.nUsers + " user(s) online.");
+                    }
+                };
+                users[key].timeout = setTimeout(cb, 5000);
+            }
+        });
+    };
+
+    // Increment a user's logon count.
+    var incrInstance = function (key) {
+        users[key].instances++;
+        console.log(key + ': ' + users[key].instances + ' instance(s)');
+        return users[key].instances;
+    };
+
+    // Decrement a user's logon count.
+    var decrInstance = function (key) {
+        users[key].instances--;
+        console.log(key + ': ' + users[key].instances + ' instance(s)');
+        return users[key].instances;
     };
 }());
